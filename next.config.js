4a65c3f80da0417dbb924cc16f64867c99625739
Ilari Schmidt <92219610+iletti@users.noreply.github.com>
2024-03-26 13:58:11 +0200

    was

--- next.config.js
+++ next.config.js
@@ -1,24 +1,31 @@
 if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `);
+  throw new Error(
+    'Please provide a valid WordPress instance URL. ' +
+    'Add to your environment variables WORDPRESS_API_URL.'
+  );
 }
 
-const { protocol, hostname, port, pathname } = new URL(
-  process.env.WORDPRESS_API_URL,
-);
+const { protocol, hostname, port, pathname } = new URL(process.env.WORDPRESS_API_URL);
 
-/** @type {import('next').NextConfig} */
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  swcMinify: true,
+  optimizeFonts: true,
   images: {
     remotePatterns: [
       {
         protocol: protocol.slice(0, -1),
         hostname,
-        port,
-        pathname: `${pathname}/**`,
+      },
+      {
+        protocol: "https",
+        hostname: "secure.gravatar.com",
       },
     ],
+    minimumCacheTTL: 15000000,
   },
-};
+}
+
+module.exports = nextConfig
