bd4f0e56a946a3aec9f7f60083c6f247103adbb6
Ilari Schmidt <92219610+iletti@users.noreply.github.com>
2024-03-28 21:22:57 +0200

    hero test

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,9 +22,9 @@ export default function Index({ allPosts: { edges }, preview }) {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="text-5xl font-bold">Hakukokeseppä Ilari Schmidt</h1>
+      <p className="py-6">Tervetuloa sivustolleni. Toteutan Google Ads projekteja näkyvyyttä kaipaaville asiakkailleni</p>
+      <button className="btn btn-primary">Katso Palvelut</button>
     </div>
   </div>
 </div>
