9b351c23fe0f7a67fc38ad6d98fd622849f7099f
Ilari Schmidt <92219610+iletti@users.noreply.github.com>
2024-03-31 20:44:30 +0300

    ip

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,8 @@ import Head from "next/head";
 import { GetStaticProps } from "next";
 import Container from "../components/container";
 import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
 import Layout from "../components/layout";
 import { getAllPostsForHome } from "../lib/api";
-import { CMS_NAME } from "../lib/constants";
 import Reference from "../components/reference";
 
 export default function Index({ allPosts: { edges }, preview }) {
