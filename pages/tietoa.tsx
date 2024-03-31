7a2a6b7669930d3efb094cd1688d01ecfde30092
Ilari Schmidt <92219610+iletti@users.noreply.github.com>
2024-03-31 10:41:20 +0300

    updt

--- pages/tietoa.tsx
+++ pages/tietoa.tsx
@@ -15,25 +15,25 @@ const About = ({ preview }) => {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Tietoa minusta</h1>
-            <p className="mb-5">  Morjes! Nimeni on Ilari Schmidt. Aloitin matkani digimarkkinoinnin parissa 2014. Plakkarissa alkaa olemaan jo iso kasa kokemusta ja toteutettuja Google Ads kampanjoita kohta jo yli 10 vuoden kokemuksella.</p>
+            <p className="mb-5 font-bold">  ⚒️ Hakukoneseppä Ilari Schmidt.</p>
           </div>
         </div>
       </div>
 
-      <div className="bg-gray-100 py-10">
+      <div className="py-10">
         <div className="container mx-auto px-4">
           <div className="mb-10">
-            <h2 className="text-center text-4xl font-bold text-gray-800 mb-6">Ammatillista taustatietoa</h2>
+            <h2 className="text-center text-4xl font-bold mb-6">Ammatillista taustatietoa</h2>
             <div className="flex flex-wrap justify-center items-center">
               <img src="https://mleuki3qgkxz.i.optimole.com/co-ZpyQ-fmwW6snM/w:auto/h:auto/q:auto/id:22e0e7b17a8f9d88025be28769af03a6/directUpload/Brown-and-Light-Brown-Circle-Framed-Instagram-Profile-Picture.png" alt="Professional portrait" className="rounded-full w-48 h-48 shadow-xl mb-6 lg:mb-0 lg:mr-8" />
-              <p className="text-xl text-gray-600 max-w-2xl">
-Yli kymmenen vuoden kokemuksella digitaalisessa markkinoinnissa erikoisosaamiseni keskittyy luomaan ja hallinnoimaan tuloksellisia Google Ads -kampanjoita. Sertifioituna Google Ads -asiantuntijana olen auttanut useita yrityksiä saavuttamaan täyden potentiaalinsa verkossa. Olipa kyseessä sitten bränditietoisuuden lisääminen tai myynnin kasvattaminen, strateginen lähestymistapani räätälöidään vastaamaan yrityksesi ainutlaatuisia tavoitteita.</p>
+              <p className="text-xl max-w-2xl">
+Yli kymmenen vuoden kokemuksella digitaalisessa markkinoinnissa erikoisosaamiseni keskittyy luomaan ja hallinnoimaan tuloksellisia Google Ads -kampanjoita. Sertifioituna Google Ads -asiantuntijana olen auttanut useita yrityksiä saavuttamaan täyden potentiaalinsa verkossa. Olipa kyseessä sitten bränditietoisuuden lisääminen tai myynnin kasvattaminen, strateginen lähestymistapani on räätälöidä toteutus vastaamaan yrityksesi yksilöllisiä tavoitteita.</p>
             </div>
           </div>
 
           <div className="my-16">
-            <h2 className="text-center text-4xl font-bold text-gray-800 mb-6">Osaaminen</h2>
-            <ul className="list-disc pl-5 text-xl text-gray-600 space-y-2">
+            <h2 className="text-center text-4xl font-bold mb-6">Osaaminen</h2>
+            <ul className="list-disc pl-5 text-xl space-y-2">
               <li>Sertifioitu Google Ads asiantuntija</li>
               <li>Osaaminen kampanjastrategiassa ja optimoinnissa</li>
               <li>Todistetusti onnistunut kasvattamaan asiakkaiden sijoitetun pääoman tuottoa (ROI)</li>
@@ -44,8 +44,8 @@ Yli kymmenen vuoden kokemuksella digitaalisessa markkinoinnissa erikoisosaamisen
           </div>
 
           <div className="text-center">
-            <h3 className="text-3xl font-bold text-gray-800 mb-6">Jutellaanko?</h3>
-            <p className="text-xl text-gray-600 mb-6">Haluatko tietää lisää siitä, miten voimme tehdä yhteistyötä? Ota yhteyttä, niin keskustellaan yrityksesi tavoitteista ja siitä, miten voin tukea kasvuasi Google Ads -palvelun avulla.</p>
+            <h3 className="text-3xl font-bold mb-6">Jutellaanko?</h3>
+            <p className="text-xl mb-6">Haluatko tietää lisää siitä, miten voimme tehdä yhteistyötä? Ota yhteyttä, niin keskustellaan yrityksesi tavoitteista ja siitä, miten voin tukea kasvuasi Google Ads -palvelun avulla.</p>
             <button className="btn btn-primary btn-lg">Ota yhteyttä</button>
           </div>
         </div>
