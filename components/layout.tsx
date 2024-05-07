9de7b06e59704de117c04b266cf6d9733b8c92d6
Ilari Schmidt <92219610+iletti@users.noreply.github.com>
2024-05-07 23:27:37 +0300

    added gtm

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,10 +2,13 @@ import Alert from "./alert";
 import Footer from "./footer";
 import Meta from "./meta";
 import Menu from "./menu";
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 export default function Layout({  preview, pageTitle, pageDescription, children }) {
   return (
     <>
+    <GoogleTagManager gtmId="GTM-KXQLCHDS" />
       <Meta  title={pageTitle} description={pageDescription}  />
       <div className="min-h-screen">
         <Alert preview={preview} />
